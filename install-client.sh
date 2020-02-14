d88620665686f9ff528c3a2f13946698654e1d77
Apocrathia <ian@apocrathia.com>
2020-02-14 08:50:07 -0700

    Update install scripts

--- install-client.sh
+++ install-client.sh
@@ -1,20 +1,18 @@
 #! /bin/bash
 
+# Prerequisites
 /usr/bin/apt-get install linux-tools-generic -y
 /sbin/modprobe vhci-hcd
 /bin/echo 'vhci-hcd' >> /etc/modules
 
 # Copy the systemd unit file
-/bin/cp ./zigbee-client.service /etc/systemd/system/
-/bin/cp ./zwave-client.service /etc/systemd/system/
+/bin/cp ./usbip-client.service /etc/systemd/system/
 
 # Reload systemd
 /bin/systemctl daemon-reload
 
 # Enable unit files
-/bin/systemctl enable zigbee-client.service
-/bin/systemctl enable zwave-client.service
+/bin/systemctl enable usbip-client.service
 
 # Start service
-/bin/systemctl start zigbee-client.service
-/bin/systemctl start zwave-client.service
+/bin/systemctl start usbip-client.service
--- install-server.sh
+++ install-server.sh
@@ -1,20 +1,18 @@
 #! /bin/bash
 
+# Prerequisites
 /usr/bin/apt-get install usbip
 /sbin/modprobe usbip_host
 /bin/echo 'usbip_host' >> /etc/modules
 
 # Copy the systemd unit file
-/bin/cp ./zigbee-server.service /etc/systemd/system/
-/bin/cp ./zwave-server.service /etc/systemd/system/
+/bin/cp ./usbip-server.service /etc/systemd/system/
 
 # Reload systemd
 /bin/systemctl daemon-reload
 
 # Enable unit files
-/bin/systemctl enable zigbee-server.service
-/bin/systemctl enable zwave-server.service
+/bin/systemctl enable usbip-server.service
 
 # Start service
-/bin/systemctl start zigbee-server.service
-/bin/systemctl start zwave-server.service
+/bin/systemctl start usbip-server.service
